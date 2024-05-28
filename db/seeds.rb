cc94297d6514345ff7ccb0ae7f71331af34a3a99
NoryBaichorov <nory.baichorov@gmail.com>
2024-05-28 17:45:53 +0300

    fix seeds v3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,76 +1,63 @@
 # frozen_string_literal: true
 
-25.times do
-  image = Rails.public_path.join('seeds/111.jpg').open
+image = Rails.public_path.join('seeds/111.jpg').open
 
-  draft = Bulletin.new(
+25.times do
+  Bulletin.create!(
     title: Faker::Name.unique.name,
     description: Faker::Markdown.emphasis,
     category_id: 1,
     user_id: 2,
-    state: :draft
+    state: :draft,
+    image:
   )
-  draft.image.attach(io: image, filename: '111.jpg', content_type: 'image/jpg')
-  draft.save!
   sleep 1
 end
 
 25.times do
-  image = Rails.public_path.join('seeds/111.jpg').open
-
-  under_moderation = Bulletin.new(
+  Bulletin.create!(
     title: Faker::Name.unique.name,
     description: Faker::Markdown.emphasis,
     category_id: 1,
     user_id: 2,
-    state: :under_moderation
+    state: :under_moderation,
+    image:
   )
-  under_moderation.image.attach(io: image, filename: '111.jpg', content_type: 'image/jpg')
-  under_moderation.save!
   sleep 1
 end
 
 25.times do
-  image = Rails.public_path.join('seeds/111.jpg').open
-
-  published = Bulletin.new(
+  Bulletin.create!(
     title: Faker::Name.unique.name,
     description: Faker::Markdown.emphasis,
     category_id: 1,
     user_id: 2,
-    state: :published
+    state: :published,
+    image:
   )
-  published.image.attach(io: image, filename: '111.jpg', content_type: 'image/jpg')
-  published.save!
   sleep 1
 end
 
 25.times do
-  image = Rails.public_path.join('seeds/111.jpg').open
-
-  rejected = Bulletin.new(
+  Bulletin.new(
     title: Faker::Name.unique.name,
     description: Faker::Markdown.emphasis,
     category_id: 1,
     user_id: 2,
-    state: :rejected
+    state: :rejected,
+    image:
   )
-  rejected.image.attach(io: image, filename: '111.jpg', content_type: 'image/jpg')
-  rejected.save!
   sleep 1
 end
 
 25.times do
-  image = Rails.public_path.join('seeds/111.jpg').open
-
-  archived = Bulletin.new(
+  Bulletin.new(
     title: Faker::Name.unique.name,
     description: Faker::Markdown.emphasis,
     category_id: 1,
     user_id: 2,
-    state: :archived
+    state: :archived,
+    image:
   )
-  archived.image.attach(io: image, filename: '111.jpg', content_type: 'image/jpg')
-  archived.save!
   sleep 1
 end
