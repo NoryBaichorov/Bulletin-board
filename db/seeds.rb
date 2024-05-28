8cfec2647d6f8d022aa14c5b200d1f1a8e6ee07e
NoryBaichorov <nory.baichorov@gmail.com>
2024-05-28 17:52:31 +0300

    fix seeds v4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,63 @@
 # frozen_string_literal: true
 
-image = Rails.public_path.join('seeds/111.jpg').open
+# image = Rails.public_path.join('seeds/111.jpg').open
 
-25.times do
-  Bulletin.create!(
-    title: Faker::Name.unique.name,
-    description: Faker::Markdown.emphasis,
-    category_id: 1,
-    user_id: 2,
-    state: :draft,
-    image:
-  )
-  sleep 1
-end
+# 25.times do
+#   Bulletin.create!(
+#     title: Faker::Name.unique.name,
+#     description: Faker::Markdown.emphasis,
+#     category_id: 1,
+#     user_id: 2,
+#     state: :draft,
+#     image:
+#   )
+#   sleep 1
+# end
 
-25.times do
-  Bulletin.create!(
-    title: Faker::Name.unique.name,
-    description: Faker::Markdown.emphasis,
-    category_id: 1,
-    user_id: 2,
-    state: :under_moderation,
-    image:
-  )
-  sleep 1
-end
+# 25.times do
+#   Bulletin.create!(
+#     title: Faker::Name.unique.name,
+#     description: Faker::Markdown.emphasis,
+#     category_id: 1,
+#     user_id: 2,
+#     state: :under_moderation,
+#     image:
+#   )
+#   sleep 1
+# end
 
-25.times do
-  Bulletin.create!(
-    title: Faker::Name.unique.name,
-    description: Faker::Markdown.emphasis,
-    category_id: 1,
-    user_id: 2,
-    state: :published,
-    image:
-  )
-  sleep 1
-end
+# 25.times do
+#   Bulletin.create!(
+#     title: Faker::Name.unique.name,
+#     description: Faker::Markdown.emphasis,
+#     category_id: 1,
+#     user_id: 2,
+#     state: :published,
+#     image:
+#   )
+#   sleep 1
+# end
 
-25.times do
-  Bulletin.new(
-    title: Faker::Name.unique.name,
-    description: Faker::Markdown.emphasis,
-    category_id: 1,
-    user_id: 2,
-    state: :rejected,
-    image:
-  )
-  sleep 1
-end
+# 25.times do
+#   Bulletin.new(
+#     title: Faker::Name.unique.name,
+#     description: Faker::Markdown.emphasis,
+#     category_id: 1,
+#     user_id: 2,
+#     state: :rejected,
+#     image:
+#   )
+#   sleep 1
+# end
 
-25.times do
-  Bulletin.new(
-    title: Faker::Name.unique.name,
-    description: Faker::Markdown.emphasis,
-    category_id: 1,
-    user_id: 2,
-    state: :archived,
-    image:
-  )
-  sleep 1
-end
+# 25.times do
+#   Bulletin.new(
+#     title: Faker::Name.unique.name,
+#     description: Faker::Markdown.emphasis,
+#     category_id: 1,
+#     user_id: 2,
+#     state: :archived,
+#     image:
+#   )
+#   sleep 1
+# end
