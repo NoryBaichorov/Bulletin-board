fb80f1c301ff3d8aa528da8002dbe281232970f5
NoryBaichorov <nory.baichorov@gmail.com>
2024-05-31 14:49:06 +0300

    fix rubocop after review

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :bulletins # rubocop:disable Rails/HasManyOrHasOneDependent
+  has_many :bulletins
 
   validates :name, presence: true
 end
